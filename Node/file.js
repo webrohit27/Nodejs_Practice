a452886f60d8695aabaaceee1f578df571d88017
rohitrodge <webrohit27@gmail.com>
2024-06-21 01:38:25 +0530

    Commit done

---
+++ Node/file.js
@@ -0,0 +1,19 @@
+ const { log } = require("console");
+const fs = require("fs"); 
+
+//  // Sync...
+//  fs.writeFileSync("./test.txt", "Hello boys");
+
+ // Async...
+// fs.writeFile("./test.txt", "Hello boys Async", (err)=>{});
+
+// const result = fs.readFileSync("./contact.txt", "utf-8")
+// console.log(result);
+
+fs.readFile("./contact.txt", "utf-8", (err, result) => {
+    if (err) {
+        console.log("error", err);
+    }else{
+        console.log(result);
+    }
+})
