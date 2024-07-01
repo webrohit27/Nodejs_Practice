b5b4bba83960d38e1f909f48c9d5c4ce01b258a1
rohitrodge <webrohit27@gmail.com>
2024-07-01 13:06:16 +0530

    commit Post

--- project-1/index.js
+++ project-1/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const users = require("./MOCK_DATA.json");
+const fs = require('fs')
 
 const app = express();
 const PORT = 8000;
+
+//Middleware - plugins
+
+app.use(express.urlencoded({extended: false}))
 
 //routes
 
@@ -28,14 +33,32 @@ app.get("/api/users", (req, res) => {
 // GET /users/1 - Get the user with ID1
 
 app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
     return res.json(user);
 });
 
 app.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+.patch((req, res) => {
+  // Edit user with id
+  return res.json({status: "pending"})
+})
+.delete((req, res) => {
+    // Edit user with id 
+    return res.json({status: "pending"});
+})
 
-
+app.post("/api/users", (req, res) => {
+     const body = req.body;
+     users.push({...body, id: users.length + 1});
+     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({status: "pending"});
+     });
+    
+});
 
 
 app.listen(PORT,() => console.log(`server started at Port:${PORT}`))
