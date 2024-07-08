4e73a966fabfa06ba6797611b3bcaea666cb8ae1
rohitrodge <webrohit27@gmail.com>
2024-07-08 21:52:39 +0530

    post done

--- project-1/index.js
+++ project-1/index.js
@@ -51,7 +51,7 @@ app.route("/api/users/:id")
     return res.json({status: "pending"});
 })
 
-app.post("/api", (req, res) => {
+app.post("/api/users", (req, res) => {
      const body = req.body;
      users.push({...body, id: users.length + 1});
      fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
