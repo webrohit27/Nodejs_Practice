74219d99b4bc443ac4969844fd55b859415c1a33
rohitrodge <webrohit27@gmail.com>
2024-07-08 21:53:50 +0530

    push users

--- project-1/index.js
+++ project-1/index.js
@@ -54,7 +54,7 @@ app.route("/api/users/:id")
 app.post("/api/users", (req, res) => {
      const body = req.body;
      users.push({...body, id: users.length + 1});
-     fs.writeFile('./MOCK_DATA.json', JSON.stringify(), (err, data) => {
+     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
         return res.json({status: "Success", id: users.length });
      });
     
